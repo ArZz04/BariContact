b45fb0308b2ac5891903f7d3bdd20c55a696720c
Juan Arvizu <arzzdeveloper04@gmail.com>
2025-01-22 15:51:16 -0600

    new

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html', // Incluye todos los archivos HTML en la raíz
+    './src/**/*.html', // O si están en la carpeta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
